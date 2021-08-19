96ae496c8badfb942a4c5cc2613fe13924f6828d
Ygor Mendes <ygortmendes@gmail.com>
2021-08-19 19:29:06 -0300

    Create mocks

---
+++ src/Mocks/lessons.ts
@@ -0,0 +1,24 @@
+export const lessons = [
+  {
+    id: "01-01",
+    exercices: [
+      {
+        id: "01-01-01",
+        name: "Escolha as palavras certas.",
+        type: "ChooseWords",
+        textToTranslate: "Hello",
+        wordsToChoose: [
+          "Olá",
+          "Café",
+          "Agua",
+          "Mãe",
+          "Irmão",
+          "Comida",
+        ],
+        successAnimation: {},
+        faillAnimation: {},
+      }
+    ]
+  },
+
+];
---
+++ src/Mocks/modules.ts
@@ -0,0 +1,23 @@
+export const modules = [
+  {
+    id: "01",
+    name: "Introdução",
+    totalLessons: 3,
+    completionPercentage: 0,
+    animation: {},
+  },
+  {
+    id: "02",
+    name: "Comprimentos",
+    totalLessons: 2,
+    completionPercentage: 0,
+    animation: {},
+  },
+  {
+    id: "03",
+    name: "Dia a dia",
+    totalLessons: 3,
+    completionPercentage: 0,
+    animation: {},
+  }
+];
